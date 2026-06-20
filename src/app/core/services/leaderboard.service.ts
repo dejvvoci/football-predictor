5efa5c1d4fb1ce92abe919d83b6d4e5ab6b2928e
Dejvi Voci <dejvivoci2@gmail.com>
2026-06-20 14:17:10 +0200

    added leaderboard page

--- src/app/core/services/leaderboard.service.ts
+++ src/app/core/services/leaderboard.service.ts
@@ -1,18 +1,20 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, query, orderBy, limit } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { UserProfile } from '../models/user.model';
 import { GroupScore } from '../models/group.model';
 
 /**
- * Implementohet plotësisht në hapin "LeaderboardService + tabela e renditjes".
+ * Leaderboard-i i grupeve implementohet plotësisht në hapin "GroupService + grupet private".
  */
 @Injectable({ providedIn: 'root' })
 export class LeaderboardService {
   private firestore = inject(Firestore);
 
   getGlobalLeaderboard(): Observable<UserProfile[]> {
-    throw new Error('TODO: query mbi users, orderBy totalPoints desc');
+    const usersRef = collection(this.firestore, 'users');
+    const q = query(usersRef, orderBy('totalPoints', 'desc'), limit(50));
+    return collectionData(q, { idField: 'uid' }) as Observable<UserProfile[]>;
   }
 
   getGroupLeaderboard(groupId: string): Observable<GroupScore[]> {
--- src/app/features/leaderboard/leaderboard.component.ts
+++ src/app/features/leaderboard/leaderboard.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { LeaderboardService } from '../../core/services/leaderboard.service';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-leaderboard',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css'
 })
 export class LeaderboardComponent {
-  // TODO: LeaderboardService.getGlobalLeaderboard()
+  private leaderboardService = inject(LeaderboardService);
+  private authService = inject(AuthService);
+
+  leaderboard$ = this.leaderboardService.getGlobalLeaderboard();
+  user$ = this.authService.user$;
 }
